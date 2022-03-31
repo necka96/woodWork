5afde02194644873232002e6e9ba593db3fb130a
necka96 <nemanja2014@live.com>
2022-03-31 15:00:06 +0200

    testemonial

--- js/main.js
+++ js/main.js
@@ -24,7 +24,8 @@ var swiper = new Swiper(".header-swiper", {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
         },
-        autoplay: true
+        autoplay: true,
+        loop: true
       });
 // counter
   var swiper = new Swiper(".home-swiper", {
@@ -55,6 +56,7 @@ var swiper = new Swiper(".header-swiper", {
           },
         },
       });
+
 $(document).ready(function () {
   $(".count").counterUp({
     delay: 10,
@@ -79,3 +81,40 @@ questions.forEach(question => {
         
   })
 })
+jQuery(document).ready(function($){
+	//create the slider
+	$('.cd-testimonials-wrapper').flexslider({
+		selector: ".cd-testimonials > li",
+		animation: "slide",
+		controlNav: false,
+		slideshow: false,
+		smoothHeight: true,
+		start: function(){
+			$('.cd-testimonials').children('li').css({
+				'opacity': 1,
+				'position': 'relative'
+			});
+		}
+	});
+
+	//open the testimonials modal page
+	$('.cd-see-all').on('click', function(){
+		$('.cd-testimonials-all').addClass('is-visible');
+	});
+
+	//close the testimonials modal page
+	$('.cd-testimonials-all .close-btn').on('click', function(){
+		$('.cd-testimonials-all').removeClass('is-visible');
+	});
+	$(document).keyup(function(event){
+		//check if user has pressed 'Esc'
+    	if(event.which=='27'){
+    		$('.cd-testimonials-all').removeClass('is-visible');	
+	    }
+    });
+    
+	//build the grid for the testimonials modal page
+	$('.cd-testimonials-all-wrapper').children('ul').masonry({
+  		itemSelector: '.cd-testimonials-item'
+	});
+});
